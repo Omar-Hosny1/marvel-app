2b878009fa459c1eb4be8a610d3d1d602752915a
Omar Hosny <honsyomar022@gmail.com>
2022-08-01 20:31:27 +0200

    API Handling Functions Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Redirect, Route } from "react-router-dom";
-import "./App.css";
+import api_properties from "./API";
 import Header from "./components/Header/Header";
 import Favorite from "./Pages/Favorite";
 import Home from "./Pages/Home";
 import MovieDetails from "./Pages/MovieDetails";
 import Movies from "./Pages/Movies";
 import News from "./Pages/News";
+import { useDispatch } from "react-redux";
+import { moviesActions } from "./store/movies-slice";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch(api_properties.API_URL)
+      .then((response) => {
+        if (!response.ok) {
+          dispatch(moviesActions.errorHandler());
+          return;
+        }
+        return response.json();
+      })
+      .then((fullData) => {
+        const moviesData = fullData.results;
+        dispatch(moviesActions.moviesStopLoadingHandler());
+        dispatch(moviesActions.pushAllMovies(moviesData));
+        return fullData.results;
+      });
+  }, []);
+
   return (
     <div className="app">
       <Header />
